3c5d4443f3833532e04a4d41abc3396381b601a9
Jeff Clement <jeff@zeos.ca>
2020-04-13 20:21:33 -0600

    game logic (#2)

--- server/models/game/game.go
+++ server/models/game/game.go
@@ -2,6 +2,7 @@ package game
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"math/rand"
 	"time"
@@ -29,18 +30,26 @@ const (
 	StateComplete
 )
 
-// GameID is the unique ID for a game
-type GameID string
+// GID is the unique ID for a game
+type GID string
 
 // PlayerID is the unique ID for a player
 type PlayerID string
+
+const rightAnswerPlayerID PlayerID = ""
+
+// RoundID is the unique ID for a round
+type RoundID string
+
+// DefinitionID is the unique ID for a definition
+type DefinitionID string
 
 // RoundState represents the status of a round
 type RoundState int
 
 const (
 	// RoundStateOpen is for a round that is open for submissions
-	RoundStateOpen State = iota
+	RoundStateOpen RoundState = iota
 	// RoundStateVoting for a round that is open for voting
 	RoundStateVoting
 	// RoundStateComplete for a completed round
@@ -49,48 +58,43 @@ const (
 
 // PlayerState represents the state of a player in the game
 type PlayerState struct {
-	ID     PlayerID
-	Name   string
-	Score  int
-	Active bool
+	ID      PlayerID
+	Name    string
+	Score   int
+	Deleted bool
 }
 
 // Definition represents a possible definition
 type Definition struct {
+	ID         DefinitionID
+	Player     PlayerID
 	Definition string
 	Votes      []PlayerID
 }
 
-// PlayerDefinition represents a definition provided by a player
-type PlayerDefinition struct {
-	Player PlayerID
-	Definition
-}
-
 // RoundData represents data collected during a round of the game
 type RoundData struct {
-	Round       int
+	ID          RoundID
 	State       RoundState
 	Word        string
-	RoundTime   time.Time
-	Definitions []Definition
+	StartTime   time.Time
+	Definitions []*Definition
 }
 
 // Game represents the state of an instance of a game
 type Game struct {
-	ID            GameID
+	ID            GID
 	State         State
 	Mode          Mode
 	RoundDuration int
-	Players       []PlayerState
+	Players       []*PlayerState
 	MaxRounds     int
-	CurrentRound  int
-	RoundData     []RoundData
+	Rounds        []*RoundData
 	StartTime     time.Time
 	wordSource    func() (word, definition string)
 }
 
-func generateGameID() GameID {
+func generateGameID() GID {
 	letters := []rune("23456789ABCDEFGHJKMNPQRSTUVWXYZ")
 	id := make([]rune, 5)
 
@@ -98,7 +102,29 @@ func generateGameID() GameID {
 		id[i] = letters[rand.Intn(len(letters))]
 	}
 
-	return GameID(id)
+	return GID(id)
+}
+
+func generateDefinitionID() DefinitionID {
+	letters := []rune("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	id := make([]rune, 10)
+
+	for i := range id {
+		id[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return DefinitionID(id)
+}
+
+func generateRoundID() RoundID {
+	letters := []rune("abcdefghijklmnopqrstuvwxyz123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	id := make([]rune, 10)
+
+	for i := range id {
+		id[i] = letters[rand.Intn(len(letters))]
+	}
+
+	return RoundID(id)
 }
 
 // GeneratePlayerID generates a unique ID for a player
@@ -121,46 +147,223 @@ func NewGame(wordSource func() (word, definition string), mode Mode, maxRounds i
 		Mode:          mode,
 		MaxRounds:     maxRounds,
 		RoundDuration: roundDuration,
-		CurrentRound:  0,
 		State:         StateNew,
 		wordSource:    wordSource,
 	}
 }
 
+func (g *Game) findPlayer(id PlayerID) (index int, player *PlayerState, err error) {
+
+	for i, p := range g.Players {
+		if p.ID == id {
+			return i, p, nil
+		}
+	}
+	return 0, nil, fmt.Errorf("player not found")
+}
+
 // AddPlayer adds a player to the game
 func (g *Game) AddPlayer(id PlayerID, name string) error {
-	// make sure player doesn't exist already
-	g.Players = append(g.Players, PlayerState{ID: id, Name: name, Active: true})
+	for _, p := range g.Players {
+		if p.ID == id {
+			return fmt.Errorf("player with this ID already part of this game")
+		}
+		if p.Name == name {
+			return fmt.Errorf("player with this name already part of this game")
+		}
+	}
+	g.Players = append(g.Players, &PlayerState{ID: id, Name: name})
 	return nil
+}
+
+// RemovePlayer adds a player to the game
+func (g *Game) RemovePlayer(id PlayerID) error {
+	_, p, err := g.findPlayer(id)
+	if err != nil || p.Deleted {
+		return fmt.Errorf("player does not exist")
+	}
+	p.Deleted = true
+	return nil
+}
+
+func (g *Game) createNewRound() *RoundData {
+	word, def := g.wordSource()
+
+	r := RoundData{
+		ID:        generateRoundID(),
+		State:     RoundStateOpen,
+		Word:      word,
+		StartTime: time.Now(),
+	}
+
+	if g.Mode == ModeNormal {
+		// in normal play mode, we append the real definition to the list of options
+		r.Definitions = append(r.Definitions, &Definition{
+			ID:         generateDefinitionID(),
+			Definition: def,
+			Player:     rightAnswerPlayerID,
+		})
+	}
+
+	return &r
 }
 
 // StartGame begins the game
 func (g *Game) StartGame() error {
-	// bail if game already started
-	// bail if no players
-	g.State = StateComplete
+	if g.State != StateNew {
+		return fmt.Errorf("game has alread been started")
+	}
+	if len(g.Players) == 0 {
+		return fmt.Errorf("starting a game requires players")
+	}
+	g.State = StateActive
 	g.StartTime = time.Now()
+
+	g.Rounds = append(g.Rounds, g.createNewRound())
+
 	return nil
+}
+
+// CurrentRound returns the current round
+func (g *Game) CurrentRound() *RoundData {
+	return g.Rounds[len(g.Rounds)-1]
 }
 
 // CloseSubmissionsForCurrentRound closes the round for new definions from players and starts voting
-func (g *Game) CloseSubmissionsForCurrentRound() error {
+func (g *Game) CloseSubmissionsForRound(round RoundID) error {
+	if g.State != StateActive {
+		return fmt.Errorf("game is not active")
+	}
+	r := g.CurrentRound()
+	if r.State != RoundStateOpen {
+		return fmt.Errorf("round is not open")
+	}
+	if r.ID != round {
+		return fmt.Errorf("not the correct round")
+	}
+	g.CurrentRound().State = RoundStateVoting
 	return nil
 }
 
-// CloseCurrentRound closes voting on the current round and tallies up the scores
-func (g *Game) CloseCurrentRound() error {
-	return nil
+func (g *Game) scoreRound() {
+	r := g.CurrentRound()
+	for _, def := range r.Definitions {
+		if def.Player == rightAnswerPlayerID {
+			// this is the right answer.  give each of these people 3 pts
+			for _, id := range def.Votes {
+				if _, p, err := g.findPlayer(id); err == nil {
+					p.Score += 3
+				}
+			}
+		} else {
+			// this is a note.  +1 to the owner
+			if _, p, err := g.findPlayer(def.Player); err == nil {
+				p.Score += len(def.Votes)
+			}
+		}
+	}
+}
 
+// CompleteCurrentRound closes voting on the current round and tallies up the scores
+func (g *Game) CompleteRound(round RoundID) error {
+	if g.State != StateActive {
+		return fmt.Errorf("game is not active")
+	}
+
+	r := g.CurrentRound()
+	if r.ID != round {
+		return fmt.Errorf("not the correct round")
+	}
+
+	r.State = RoundStateComplete
+	g.scoreRound()
+
+	if len(g.Rounds) < g.MaxRounds {
+		// if we aren't at maxRounds, spin up a new game
+		newRound := g.createNewRound()
+		g.Rounds = append(g.Rounds, newRound)
+	} else {
+		g.EndGame()
+	}
+
+	return nil
 }
 
 // SubmitWord submits a definition
-func (g *Game) SubmitWord(player PlayerID, round int, definition string) error {
+func (g *Game) SubmitWord(player PlayerID, round RoundID, definition string) error {
+	if g.State != StateActive {
+		return fmt.Errorf("game is not active")
+	}
+	r := g.CurrentRound()
+	if r.State != RoundStateOpen {
+		return fmt.Errorf("round is not open")
+	}
+	if r.ID != round {
+		return fmt.Errorf("not the correct round")
+	}
+	if _, p, err := g.findPlayer(player); err != nil || p.Deleted {
+		return fmt.Errorf("player not found")
+	}
+	for _, def := range r.Definitions {
+		if def.Player == player {
+			return fmt.Errorf("player already submitted a definition for this round")
+		}
+	}
+	r.Definitions = append(r.Definitions, &Definition{
+		ID:         generateDefinitionID(),
+		Player:     player,
+		Definition: definition,
+	})
+	return nil
+}
+
+// Vote casts a vote for a player
+func (g *Game) Vote(player PlayerID, round RoundID, definition DefinitionID) error {
+	if g.State != StateActive {
+		return fmt.Errorf("game is not active")
+	}
+	r := g.CurrentRound()
+	if r.State != RoundStateVoting {
+		return fmt.Errorf("round is not voting")
+	}
+	if r.ID != round {
+		return fmt.Errorf("not the correct round")
+	}
+	if _, p, err := g.findPlayer(player); err != nil || p.Deleted {
+		return fmt.Errorf("player not found")
+	}
+	for _, def := range r.Definitions {
+		for _, vote := range def.Votes {
+			if vote == player {
+				return fmt.Errorf("player already voted")
+			}
+		}
+	}
+	for _, def := range r.Definitions {
+		if def.ID == definition {
+			if def.Player == player {
+				return fmt.Errorf("can't vote for self")
+			}
+			def.Votes = append(def.Votes, player)
+		}
+	}
 	return nil
 }
 
 // EndGame sets a game status to complete
 func (g *Game) EndGame() error {
+	if g.State == StateComplete {
+		return fmt.Errorf("game is already complete")
+	}
+
+	if g.State == StateActive {
+		r := g.CurrentRound()
+		if r.State != RoundStateComplete {
+			r.State = RoundStateComplete
+			g.scoreRound()
+		}
+	}
+
 	g.State = StateComplete
 	return nil
 }
@@ -169,6 +372,7 @@ func (g *Game) EndGame() error {
 func (g *Game) Serialize(writer io.Writer) {
 	enc := json.NewEncoder(writer)
 	enc.Encode(g)
+	writer.Write([]byte("test"))
 }
 
 // Unserialize a game from a reader
--- server/models/game/game_test.go
+++ server/models/game/game_test.go
@@ -1,12 +1,14 @@
 package game
 
 import (
+	"bufio"
+	"bytes"
 	"strings"
 	"testing"
 )
 
 func TestGameIDUnique(t *testing.T) {
-	seen := make(map[GameID]bool)
+	seen := make(map[GID]bool)
 	for i := 0; i < 100; i++ {
 		id := generateGameID()
 		if _, exists := seen[id]; exists {
@@ -48,5 +50,390 @@ func TestPlayerIDUnique(t *testing.T) {
 func TestPlayerIDSize(t *testing.T) {
 	if len(GeneratePlayerID()) != 20 {
 		t.Error("Generated Player IDs must be 20 characters")
+	}
+}
+
+func testableWordGenerator() func() (word, definition string) {
+	i := 0
+	words := []string{"hello", "world"}
+	return func() (word string, definition string) {
+		if i == len(words) {
+			i = 0
+		}
+		w := words[i]
+		i++
+		return w, "definition of " + w
+	}
+}
+
+func TestStartGameNoPlayers(t *testing.T) {
+	g := NewGame(testableWordGenerator(), ModeNormal, 3, 600)
+	if err := g.StartGame(); err == nil {
+		t.Error("Starting a game with no players should fail")
+	}
+}
+
+func TestStartGameAlreadyStarted(t *testing.T) {
+	g := NewGame(testableWordGenerator(), ModeNormal, 3, 600)
+	for _, state := range []State{StateActive, StateComplete} {
+		g.State = state
+		if err := g.StartGame(); err == nil {
+			t.Errorf("Game in state '%d' should fail", state)
+		}
+	}
+}
+
+func TestStartGame(t *testing.T) {
+	g := NewGame(testableWordGenerator(), ModeNormal, 3, 600)
+	if g.State != StateNew {
+		t.Errorf("Game should start in New state")
+	}
+	g.AddPlayer(GeneratePlayerID(), "Tester")
+	if err := g.StartGame(); err != nil {
+		t.Errorf("Game should have started")
+	}
+	if g.State != StateActive {
+		t.Error("Game state should be active")
+	}
+	if g.StartTime.IsZero() {
+		t.Error("Start time should be set")
+	}
+	if len(g.Rounds) != 1 {
+		t.Error("There should be a round!")
+	}
+}
+
+func TestCreateNewRoundNormal(t *testing.T) {
+	g := NewGame(testableWordGenerator(), ModeNormal, 3, 600)
+	r := g.createNewRound()
+	if r.State != RoundStateOpen {
+		t.Error("Rounds should start as open")
+	}
+	if r.Word != "hello" {
+		t.Error("Expecting 'hello' from the generator")
+	}
+	if len(r.Definitions) != 1 {
+		t.Error("Expecting a single definition")
+	} else {
+		if r.Definitions[0].Definition != "definition of hello" {
+			t.Error("Expecting 'definition of hello' from the generator")
+		}
+	}
+}
+
+func TestCreateNewRoundFun(t *testing.T) {
+
+	g := NewGame(testableWordGenerator(), ModeFun, 3, 600)
+	r := g.createNewRound()
+	if r.State != RoundStateOpen {
+		t.Error("Rounds should start as open")
+	}
+	if r.Word != "hello" {
+		t.Error("Expecting 'hello' from the generator")
+	}
+	if len(r.Definitions) != 0 {
+		t.Error("Expecting a no definitions")
+	}
+}
+
+func TestCloseSubmissionsForCurrentRound(t *testing.T) {
+	g := NewGame(testableWordGenerator(), ModeNormal, 3, 600)
+	g.AddPlayer(GeneratePlayerID(), "Tester")
+	if err := g.CloseSubmissionsForRound(RoundID("")); err == nil {
+		t.Error("Can't close submissions on an inactive game")
+		return
+	}
+	g.StartGame()
+	if err := g.CloseSubmissionsForRound(RoundID("Bad ID")); err == nil {
+		t.Error("Bad Round ID")
+		return
+	}
+	if err := g.CloseSubmissionsForRound(g.CurrentRound().ID); err != nil {
+		t.Error("Should have been able to close a new round")
+		return
+	}
+	if g.Rounds[0].State != RoundStateVoting {
+		t.Error("Round should be open for voting now")
+		return
+	}
+	if err := g.CloseSubmissionsForRound(g.CurrentRound().ID); err == nil {
+		t.Error("Should have been able to close a voting round")
+		return
+	}
+}
+
+func TestCompleteCurrentRoundBeforeVoting(t *testing.T) {
+	g := NewGame(testableWordGenerator(), ModeNormal, 3, 600)
+	g.AddPlayer(GeneratePlayerID(), "Tester")
+	if err := g.CompleteRound(RoundID("")); err == nil {
+		t.Error("Can't complete a round on an inactive game")
+		return
+	}
+	g.StartGame()
+	if err := g.CompleteRound(RoundID("Bad Round ID")); err == nil {
+		t.Error("Bad Round ID")
+		return
+	}
+	if err := g.CompleteRound(g.CurrentRound().ID); err != nil {
+		t.Error("Should have been able to close a new round")
+		return
+	}
+	if g.Rounds[0].State != RoundStateComplete {
+		t.Error("Round should be completed now")
+		return
+	}
+	if len(g.Rounds) != 2 {
+		t.Error("Should have started a new round")
+		return
+	}
+}
+
+func TestCompleteCurrentRoundAfterVoting(t *testing.T) {
+	g := NewGame(testableWordGenerator(), ModeNormal, 3, 600)
+	g.AddPlayer(GeneratePlayerID(), "Tester")
+	if err := g.CompleteRound(RoundID("")); err == nil {
+		t.Error("Can't complete a round on an inactive game")
+		return
+	}
+	g.StartGame()
+	rid := g.CurrentRound().ID
+	// add some votes in here for scoring
+	if err := g.CloseSubmissionsForRound(rid); err != nil {
+		t.Error("Should have been able to close this!")
+		return
+	}
+	if err := g.CompleteRound(rid); err != nil {
+		t.Error("Should have been able to close a new round")
+		return
+	}
+	if g.Rounds[0].State != RoundStateComplete {
+		t.Error("Round should be completed now")
+		return
+	}
+}
+
+func TestCompleteCurrentRoundEndsGame(t *testing.T) {
+	g := NewGame(testableWordGenerator(), ModeNormal, 2, 600)
+	g.AddPlayer(GeneratePlayerID(), "Tester")
+	g.StartGame()
+	if len(g.Rounds) != 1 {
+		t.Error("Expecting 1 round")
+	}
+	rid := g.CurrentRound().ID
+	g.CompleteRound(rid)
+	if len(g.Rounds) != 2 {
+		t.Error("Expecting 2 rounds")
+	}
+	rid = g.CurrentRound().ID
+	g.CompleteRound(rid)
+	if len(g.Rounds) != 2 {
+		t.Error("Expecting 2 rounds")
+	}
+	if g.State != StateComplete {
+		t.Error("Expecting game to be complete")
+	}
+
+}
+
+func TestAddingPlayers(t *testing.T) {
+	p1id := GeneratePlayerID()
+	p2id := GeneratePlayerID()
+	g := NewGame(testableWordGenerator(), ModeNormal, 2, 600)
+	if err := g.AddPlayer(p1id, "Tester 1"); err != nil {
+		t.Error("Adding player should succeed here")
+		return
+	}
+	if err := g.AddPlayer(p1id, "Tester 2"); err == nil {
+		t.Error("Adding player should fail here")
+		return
+	}
+	if err := g.AddPlayer(p2id, "Tester 1"); err == nil {
+		t.Error("Adding player should fail here")
+		return
+	}
+	if err := g.AddPlayer(p2id, "Tester 2"); err != nil {
+		t.Error("Adding player should succeed here")
+		return
+	}
+	if _, p, err := g.findPlayer(p1id); err != nil || p.Name != "Tester 1" || p.ID != p1id {
+		t.Error("Can't find Tester 1")
+	}
+	if _, p, err := g.findPlayer(p2id); err != nil || p.Name != "Tester 2" || p.ID != p2id {
+		t.Error("Can't find Tester 2")
+	}
+	if _, p, err := g.findPlayer("bad user"); err == nil || p != nil {
+		t.Error("Shouldn't find non-existant user")
+	}
+
+}
+
+func TestRemovePlayer(t *testing.T) {
+	p1id := GeneratePlayerID()
+	g := NewGame(testableWordGenerator(), ModeNormal, 2, 600)
+	if err := g.AddPlayer(p1id, "Tester 1"); err != nil {
+		t.Error("Adding player should succeed here")
+		return
+	}
+	_, p, _ := g.findPlayer(p1id)
+	if p.Deleted {
+		t.Error("Player shouldn't be deleted yet")
+		return
+	}
+	if err := g.RemovePlayer(p1id); err != nil {
+		t.Error("Should be able to remove this")
+		return
+	}
+	if !p.Deleted {
+		t.Error("Now he should")
+		return
+	}
+	if err := g.RemovePlayer(p1id); err == nil {
+		t.Error("Shouldn't be able to remove this twice")
+		return
+	}
+	if err := g.RemovePlayer("bad user"); err == nil {
+		t.Error("Shouldn't be able to non-existant user")
+		return
+	}
+}
+
+func TestSerializeDeserialize(t *testing.T) {
+	g := NewGame(testableWordGenerator(), ModeNormal, 2, 600)
+	g.AddPlayer(PlayerID("123"), "Bob")
+	g.StartGame()
+	var b bytes.Buffer
+	w := bufio.NewWriter(&b)
+	g.Serialize(w)
+	w.Flush()
+	t.Log(b.String())
+
+	var g2 Game
+	if err := Unserialize(bufio.NewReader(&b), &g2); err != nil {
+		t.Error("Failed to deserialize")
+	}
+
+	if g.ID != g2.ID {
+		t.Error("Not the same game back")
+	}
+}
+
+func TestSubmitWord(t *testing.T) {
+	id := GeneratePlayerID()
+	g := NewGame(testableWordGenerator(), ModeNormal, 2, 600)
+	g.AddPlayer(id, "Tester")
+	if err := g.SubmitWord(id, RoundID(""), "My Def"); err == nil {
+		t.Error("Can't submit words on inactive game")
+	}
+	g.StartGame()
+	rid := g.CurrentRound().ID
+	if err := g.SubmitWord(GeneratePlayerID(), rid, "My Def"); err == nil {
+		t.Error("Non-existent users can't submit words")
+	}
+	if err := g.SubmitWord(id, RoundID("Bad ID"), "My Def"); err == nil {
+		t.Error("Bad Round ID")
+	}
+	if err := g.SubmitWord(id, rid, "My Def"); err != nil {
+		t.Error("Submit should have worked here")
+	}
+	if err := g.SubmitWord(id, rid, "My Def"); err == nil {
+		t.Error("No double submissions")
+	}
+	if len(g.CurrentRound().Definitions) != 2 {
+		t.Error("Should be two definitions here")
+	}
+	if g.CurrentRound().Definitions[1].Player != id || g.CurrentRound().Definitions[1].Definition != "My Def" {
+		t.Error("This should be my definition")
+	}
+	g.CloseSubmissionsForRound(rid)
+	if err := g.SubmitWord(id, rid, "My Def"); err == nil {
+		t.Error("Can't submit while voting")
+	}
+}
+
+func TestVoting(t *testing.T) {
+	id1 := GeneratePlayerID()
+	id2 := GeneratePlayerID()
+	g := NewGame(testableWordGenerator(), ModeNormal, 2, 600)
+	g.AddPlayer(id1, "Tester 1")
+	g.AddPlayer(id2, "Tester 2")
+	if err := g.Vote(id1, RoundID(""), DefinitionID("")); err == nil {
+		t.Error("Can't submit words on inactive game")
+	}
+	g.StartGame()
+	roundID := g.CurrentRound().ID
+	g.SubmitWord(id1, roundID, "a")
+	g.SubmitWord(id2, roundID, "b")
+	if len(g.CurrentRound().Definitions) != 3 {
+		t.Error("Not enought definitions")
+	}
+	if err := g.Vote(id1, roundID, DefinitionID("")); err == nil {
+		t.Error("Can't submit words on inactive rounds")
+	}
+	g.CloseSubmissionsForRound(roundID)
+
+	defCorrect := g.CurrentRound().Definitions[0].ID
+	def1 := g.CurrentRound().Definitions[1]
+	def2 := g.CurrentRound().Definitions[2]
+
+	t.Log("def1", id1, def1)
+	t.Log("def2", id2, def2)
+
+	if err := g.Vote(PlayerID("bogus"), roundID, def1.ID); err == nil {
+		t.Error("Non-existend users can't vote")
+	}
+	if err := g.Vote(id1, roundID, def1.ID); err == nil {
+		t.Error("Can't vote for self")
+	}
+	if err := g.Vote(id1, RoundID("not real"), def2.ID); err == nil {
+		t.Error("this shouldn't work.  bad round ID")
+	}
+	if err := g.Vote(id1, roundID, def2.ID); err != nil {
+		t.Error("Vote should have worked")
+	}
+	if err := g.Vote(id1, roundID, def2.ID); err == nil {
+		t.Error("Can't vote twice")
+	}
+	if err := g.Vote(id1, roundID, defCorrect); err == nil {
+		t.Error("Can't vote twice")
+	}
+	if err := g.Vote(id2, roundID, defCorrect); err != nil {
+		t.Error("Should have worked")
+	}
+	g.CompleteRound(roundID)
+
+	if _, p, err := g.findPlayer(id1); err == nil {
+		if p.Score != 0 {
+			t.Error("Player 1 score should be zero")
+		}
+	}
+
+	if _, p, err := g.findPlayer(id2); err == nil {
+		if p.Score != 4 {
+			t.Error("Player 2 score should be four")
+		}
+	}
+
+}
+
+func TestEndGame(t *testing.T) {
+	id1 := GeneratePlayerID()
+	g := NewGame(testableWordGenerator(), ModeNormal, 2, 600)
+	g.AddPlayer(id1, "Tester 1")
+	g.StartGame()
+	rid := g.CurrentRound().ID
+	g.SubmitWord(id1, rid, "a")
+	g.CloseSubmissionsForRound(rid)
+	defCorrect := g.CurrentRound().Definitions[0].ID
+	g.Vote(id1, rid, defCorrect)
+	if err := g.EndGame(); err != nil {
+		t.Error("This should work")
+	}
+	if _, p, err := g.findPlayer(id1); err == nil {
+		if p.Score != 3 {
+			t.Error("Player 1 score should be three")
+		}
+	}
+	if err := g.EndGame(); err == nil {
+		t.Error("This should not work")
 	}
 }
