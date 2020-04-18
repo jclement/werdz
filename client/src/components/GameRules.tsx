705fb5baa7478b7b3c040d2fea70e56a5852e527
Jeff Clement <jeff@zeos.ca>
2020-04-18 13:02:57 -0600

    game rules

--- client/src/components/GameRules.tsx
+++ client/src/components/GameRules.tsx
@@ -8,12 +8,30 @@ interface GameRulesProps {
 export class GameRules extends Component<GameRulesProps, any> {
     render() {
         return (
-            <div>
+            <div style={{paddingTop: "20px"}}>
                 <div className="card">
                     <div className="card-header">
                         <h4>Rules for this game</h4>
                     </div>
                     <div className="card-body">
+                        {this.props.mode === 0 && <ul>
+                            <li>This game will consist of <b>{this.props.rounds}</b> rounds.</li>
+                            <li>In each round, you will be presented with an unusual <b>but real</b> word.</li>
+                            <li>You will have to make up a definition that you think your friends will believe is the correct definition (hint: the real definitions are usually fairly short such as 'room for conversation').</li>
+                            <li>Once everybody has submitted their best guess for what the word means, you'll be presented with a shuffled list of everybody's definitions and the correct definition.</li>
+                            <li>You get one point for each of your friends who mistakenly pick your definition as the correct definition.</li>
+                            <li>You get three points if you can correctly identify the real definition.</li>
+                            <li>At the end of {this.props.rounds} rounds, the player with the most points is the winner.</li>
+                            <li>That's it.  Have fun. </li>
+                        </ul>}
+                        {this.props.mode === 1 && <ul>
+                            <li>This game will consist of <b>{this.props.rounds}</b> rounds.</li>
+                            <li>In each round, you will be presented with a crazy random word.</li>
+                            <li>You will have to make up an outlandish definition for the crazy random word.  Something that's so hilarious and awesome that your friends have to vote for it!</li>
+                            <li>You get one point for each vote your crazy definition gets.</li>
+                            <li>At the end of {this.props.rounds} rounds, the player with the most points is the winner.</li>
+                            <li>That's it.  Have fun. </li>
+                        </ul>}
                     </div>
                 </div>
 
